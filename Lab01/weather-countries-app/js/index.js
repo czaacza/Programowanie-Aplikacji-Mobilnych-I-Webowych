9c015608c1a364c8d701ec54e59c6d127c8438a3
Mati <maticzacza@gmail.com>
2023-10-09 20:00:56 +0200

    ip addres seach

--- Lab01/weather-countries-app/js/index.js
+++ Lab01/weather-countries-app/js/index.js
@@ -1,9 +1,10 @@
-continentButtons = document.querySelectorAll('.continent-btn');
-
-continentButtons.forEach(function (button) {
-  button.addEventListener('click', function () {
-    value = button.getAttributeNode('value').value;
-    const url = `http://dataservice.accuweather.com/locations/v1/countries/${value}?apikey=dvT5HgUQ1B4sARqRSNudKCWvZOVRxHjy&language=en-us`;
+const setSearchFormListener = function () {
+  const searchForm = document.querySelector('.search-container form');
+  searchForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const searchInput = document.querySelector('.search-container input');
+    const searchValue = searchInput.value;
+    const url = `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=dvT5HgUQ1B4sARqRSNudKCWvZOVRxHjy&q=${searchValue}`;
     fetch(url)
       .then(function (response) {
         return response.json();
@@ -11,20 +12,68 @@ continentButtons.forEach(function (button) {
       .then(function (data) {
         // Use the data to update the page
         console.log(data);
-        displayCountries(data);
-        setCountryButtonListeners();
+        displaySearchResults(data);
       })
       .catch(function (err) {
         console.log(err);
       });
   });
-});
+};
 
-function displayCountries(countries) {
+const setContinentButtonListeners = function () {
+  continentButtons = document.querySelectorAll('.continent-btn');
+  console.log(continentButtons);
+  continentButtons.forEach(function (button) {
+    button.addEventListener('click', function () {
+      value = button.getAttributeNode('value').value;
+      const url = `http://dataservice.accuweather.com/locations/v1/countries/${value}?apikey=dvT5HgUQ1B4sARqRSNudKCWvZOVRxHjy&language=en-us`;
+      fetch(url)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          // Use the data to update the page
+          console.log(data);
+          displayCountries('country', data);
+          setCountryButtonListeners();
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    });
+  });
+};
+
+const setCountryButtonListeners = function () {
+  countryButtons = document.querySelectorAll('.country-btn');
+
+  countryButtons.forEach(function (button) {
+    // send a get request to get the data from a sample url
+    button.addEventListener('click', function () {
+      value = button.getAttributeNode('value').value;
+      const url = `http://dataservice.accuweather.com/locations/v1/adminareas/${value}?apikey=dvT5HgUQ1B4sARqRSNudKCWvZOVRxHjy`;
+      fetch(url)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          displayCountries('region', data);
+          console.log(data);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    });
+  });
+};
+
+function displayCountries(title, countries) {
   const container = document.querySelector('#countries-container');
   container.innerHTML = '';
 
-  const title = document.getElementById('countries-title');
+  const countriesTitle = document.getElementById('countries-title');
+  countriesTitle.classList.remove('hidden');
+  countriesTitle.innerText = `Choose your ${title}`;
 
   countries.forEach((country) => {
     const card = document.createElement('div');
@@ -41,25 +90,28 @@ function displayCountries(countries) {
   });
 }
 
-const setCountryButtonListeners = function () {
-  countryButtons = document.querySelectorAll('.country-btn');
+function displaySearchResults(results) {
+  const container = document.querySelector('#countries-container');
+  container.innerHTML = '';
 
-  countryButtons.forEach(function (button) {
-    // send a get request to get the data from a sample url
-    button.addEventListener('click', function () {
-      value = button.getAttributeNode('value').value;
-      const url = `http://dataservice.accuweather.com/locations/v1/adminareas/${value}?apikey=dvT5HgUQ1B4sARqRSNudKCWvZOVRxHjy`;
-      fetch(url)
-        .then(function (response) {
-          return response.json();
-        })
-        .then(function (data) {
-          displayCountries(data);
-          console.log(data);
-        })
-        .catch(function (err) {
-          console.log(err);
-        });
-    });
+  const countriesTitle = document.getElementById('countries-title');
+  countriesTitle.classList.remove('hidden');
+  countriesTitle.innerText = `Search Results`;
+
+  results.forEach((result) => {
+    const card = document.createElement('div');
+    card.setAttribute('value', result.Key);
+
+    card.className = 'card';
+    card.classList.add('country-btn');
+
+    const cardText = document.createElement('p');
+    cardText.innerText = result.LocalizedName;
+
+    card.appendChild(cardText);
+    container.appendChild(card);
   });
-};
+}
+
+setContinentButtonListeners();
+setSearchFormListener();
