8d77a2add7ad5bc673d68c64ff046e3478fcb257
Mati <maticzacza@gmail.com>
2023-10-09 14:54:20 +0200

    country regions done

--- Lab01/weather-countries-app/js/index.js
+++ Lab01/weather-countries-app/js/index.js
@@ -1,10 +1,8 @@
 continentButtons = document.querySelectorAll('.continent-btn');
 
 continentButtons.forEach(function (button) {
-  // Add event listener to each button
   button.addEventListener('click', function () {
     value = button.getAttributeNode('value').value;
-    // create an api get request to get the data from a sample url
     const url = `http://dataservice.accuweather.com/locations/v1/countries/${value}?apikey=dvT5HgUQ1B4sARqRSNudKCWvZOVRxHjy&language=en-us`;
     fetch(url)
       .then(function (response) {
@@ -14,6 +12,7 @@ continentButtons.forEach(function (button) {
         // Use the data to update the page
         console.log(data);
         displayCountries(data);
+        setCountryButtonListeners();
       })
       .catch(function (err) {
         console.log(err);
@@ -23,10 +22,16 @@ continentButtons.forEach(function (button) {
 
 function displayCountries(countries) {
   const container = document.querySelector('#countries-container');
+  container.innerHTML = '';
+
+  const title = document.getElementById('countries-title');
 
   countries.forEach((country) => {
     const card = document.createElement('div');
+    card.setAttribute('value', country.ID);
+
     card.className = 'card';
+    card.classList.add('country-btn');
 
     const cardText = document.createElement('p');
     cardText.innerText = country.EnglishName;
@@ -35,3 +40,26 @@ function displayCountries(countries) {
     container.appendChild(card);
   });
 }
+
+const setCountryButtonListeners = function () {
+  countryButtons = document.querySelectorAll('.country-btn');
+
+  countryButtons.forEach(function (button) {
+    // send a get request to get the data from a sample url
+    button.addEventListener('click', function () {
+      value = button.getAttributeNode('value').value;
+      const url = `http://dataservice.accuweather.com/locations/v1/adminareas/${value}?apikey=dvT5HgUQ1B4sARqRSNudKCWvZOVRxHjy`;
+      fetch(url)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          displayCountries(data);
+          console.log(data);
+        })
+        .catch(function (err) {
+          console.log(err);
+        });
+    });
+  });
+};
